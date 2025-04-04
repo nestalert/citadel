ef5699a49888596ad98c7c7b59e5bb254055a6be
killsun245 <gregorzaf5@gmail.com>
2025-04-04 18:46:17 +0300

    fixed animation

--- Citadel/Assets/PlayerMovement.cs
+++ Citadel/Assets/PlayerMovement.cs
@@ -18,23 +18,8 @@ public class PlayerMovement : MonoBehaviour
         movement = Vector2.zero;
 
         // Input
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            movement.x = 1f;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            movement.x = -1f;
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-        {
-            movement.y = 1f;
-        }
-        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-        {
-            movement.y = -1f;
-        }
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
         // Normalize diagonal movement to prevent faster speed
         if (movement.magnitude > 1)
@@ -42,20 +27,11 @@ public class PlayerMovement : MonoBehaviour
             movement = movement.normalized;
         }
 
-        // Update Animator parameters only when there is input
-        if (movement.sqrMagnitude > 0)
-        {
-            animator.SetFloat("Horizontal", movement.x);
-            animator.SetFloat("Vertical", movement.y);
-            animator.SetFloat("IsMoving", 1f); // Set IsMoving to true when there's movement
-        }
-        else
-        {
-            animator.SetFloat("IsMoving", 0f); // Set IsMoving to false when no movement
-        }
-
-        // You might not need a separate "Speed" float if you're using IsMoving
-        // animator.SetFloat("Speed", movement.sqrMagnitude);
+        //animation
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        
     }
 
     void FixedUpdate()
